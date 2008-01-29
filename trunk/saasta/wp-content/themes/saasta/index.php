dcbcd0c5e0e9254c3badf3e71bef5c28d9e87aaa
jjhellst <jjhellst@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-01-29 22:42:14 +0000

    Enable tag viewing in saasta theme

--- trunk/saasta/wp-content/themes/saasta/index.php
+++ trunk/saasta/wp-content/themes/saasta/index.php
@@ -22,7 +22,7 @@ if (isset($_REQUEST['tagsor'])) {
 	saasta_print_tags();
 }
 ?>
-				<p class="postmetadata">Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
+				<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
 			</div>
 
 		<?php endwhile; ?>
