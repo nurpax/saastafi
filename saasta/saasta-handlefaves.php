a6be6f7b2f1eaa1c895309121c582023e4c0e926
mikko.uromo <mikko.uromo@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-02-02 14:36:40 +0000

    draft of informing users of faves to their posts via e-mail

--- saasta/saasta-handlefaves.php
+++ saasta/saasta-handlefaves.php
@@ -17,6 +17,17 @@ $user = wp_get_current_user();
 if ( $user->ID ) {
 	if ($add) {
 		$wpdb->query("insert ignore into saasta_faves (user_id,post_id) values (".$user->ID.",".$postId.")");
+
+		// TODO: uncomment this when you have a mail server set up
+		// TODO: might have to set fourth parameter as "From: some address"
+		/*
+		$post = get_post($postId, OBJECT);
+		$author = get_userdata($post->post_author);
+
+		$msg = "Your saasta '".$post->post_title."' (".$post->guid.") was just marked as a fave by ".$user->user_login."!";
+
+		mail($author->user_email, "[saasta.fi] - your post got faved!", $msg);
+		*/
 	}
 	else {
 		$wpdb->query("delete from saasta_faves where user_id=".$user->ID." and post_id=".$postId);
