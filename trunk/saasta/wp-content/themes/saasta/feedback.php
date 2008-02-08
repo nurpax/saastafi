11af373ffe0619b9ced50c9f09eee630dc4d2c48
mikko.uromo <mikko.uromo@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-02-08 17:07:48 +0000

    extra linebreaks to make layout prettier

--- trunk/saasta/wp-content/themes/saasta/feedback.php
+++ trunk/saasta/wp-content/themes/saasta/feedback.php
@@ -14,8 +14,8 @@ function saasta_print_feedback_form()
 
     print '<form action="'.get_option('siteurl').'/saasta-feedback.php" method="post">';
     print '<input type="hidden" name="redirect_to" value="'.$redirectURI.'"/>';
-    print '<strong>Subject: </strong><input size="70" type="text" name="subject"/><br/><br/>';
-    print '<strong>Text:</strong><textarea rows="20" cols="60" name="body"></textarea>';
+    print '<strong>Subject: </strong><br/><input size="70" type="text" name="subject"/><br/><br/>';
+    print '<strong>Text:</strong><br/><textarea rows="20" cols="60" name="body"></textarea>';
     print '<br />';
     print '<input type="submit" style="border:1px solid black;font-size:smaller;background-color:#ddd391" value="Send"/>';
     print '</form>';
