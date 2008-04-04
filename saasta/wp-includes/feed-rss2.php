d67e386b4bfa8470cd9a51e06adccac68b198320
jjhellst <jjhellst@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-04-04 15:46:18 +0000

    Upgrade to WordPress 2.5

--- saasta/wp-includes/feed-rss2.php
+++ saasta/wp-includes/feed-rss2.php
@@ -1,41 +1,36 @@
 <?php
+/**
+ * RSS2 Feed Template for displaying RSS2 Posts feed.
+ *
+ * @package WordPress
+ */
+
 header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
 $more = 1;
 
 ?>
 <?php echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 
-<!-- generator="wordpress/<?php bloginfo_rss('version') ?>" -->
 <rss version="2.0"
 	xmlns:content="http://purl.org/rss/1.0/modules/content/"
 	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
 	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
 	<?php do_action('rss2_ns'); ?>
 >
 
 <channel>
 	<title><?php bloginfo_rss('name'); wp_title_rss(); ?></title>
+	<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
 	<link><?php bloginfo_rss('url') ?></link>
 	<description><?php bloginfo_rss("description") ?></description>
 	<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT'), false); ?></pubDate>
-	<generator>http://wordpress.org/?v=<?php bloginfo_rss('version'); ?></generator>
+	<?php the_generator( 'rss2' ); ?>
 	<language><?php echo get_option('rss_language'); ?></language>
 	<?php do_action('rss2_head'); ?>
 	<?php while( have_posts()) : the_post(); ?>
 	<item>
-
-       <title><?php the_title_rss() ?><?php
-    $arr = get_the_category();
-    $catStr = "";
-    for ($i = 0, $j = 0; $i < count($arr); $i++) {
-        if (strcmp($arr[$i]->name, "saasta") < 0) {
-            if ($j > 0) $catStr .= ", ";
-            $catStr .= trim($arr[$i]->name);
-            $j++;
-        }
-     }
-    if ($catStr != "") print (" (" . $catStr . ")"); ?></title>
-
+		<title><?php the_title_rss() ?></title>
 		<link><?php the_permalink_rss() ?></link>
 		<comments><?php comments_link(); ?></comments>
 		<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
@@ -48,12 +43,12 @@ $more = 1;
 <?php else : ?>
 		<description><![CDATA[<?php the_excerpt_rss() ?>]]></description>
 	<?php if ( strlen( $post->post_content ) > 0 ) : ?>
-			 <content:encoded><![CDATA[<?php the_content() ?> <?php if (get_the_tag_list()) print get_the_tag_list('<p>(Tags: ',', ',')</p>'); ?>]]></content:encoded>
+		<content:encoded><![CDATA[<?php the_content() ?>]]></content:encoded>
 	<?php else : ?>
 		<content:encoded><![CDATA[<?php the_excerpt_rss() ?>]]></content:encoded>
 	<?php endif; ?>
 <?php endif; ?>
-		<wfw:commentRss><?php echo comments_rss(); ?></wfw:commentRss>
+		<wfw:commentRss><?php echo get_post_comments_feed_link(); ?></wfw:commentRss>
 <?php rss_enclosure(); ?>
 	<?php do_action('rss2_item'); ?>
 	</item>
