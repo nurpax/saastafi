ca8338336c990763f068763f736a61cfde9046c9
mikko.uromo <mikko.uromo@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-04-04 17:48:30 +0000

    new shit

--- saasta/wp-saasta-stats.php
+++ saasta/wp-saasta-stats.php
@@ -1,6 +1,15 @@
 <?php
-require(dirname(__FILE__) . '/wp-blog-header.php');
 
+if (empty($wp)) {
+	require_once('./wp-config.php');
+ }
+
+get_currentuserinfo();
+// no user
+if ('' == $user_ID)
+	return;
+
+/*
 function query_total_num_posts ()
 {
     global $wpdb;
@@ -15,27 +24,77 @@ WHERE
 
     return $q_stats;
 }
+*/
 
+function query_num_posts_in_quarter($q, $y) {
+	global $wpdb;
 
-		    $n_posts = query_total_num_posts();
+	$query = "SELECT su.display_name as name, count(sp.ID) as num_posts
+FROM saasta_posts sp, 
+     saasta_users su 
+WHERE sp.post_status='publish' AND 
+      su.ID=sp.post_author AND
+      QUARTER(sp.post_date)=".$q." AND
+      YEAR(sp.post_date)=".$y."
+GROUP BY 
+      su.ID 
+ORDER BY 
+      num_posts desc, 
+      name";
+
+    return $wpdb->get_results($query);
+}
+
+//$n_posts = query_total_num_posts();
+
 ?>
 
 <html>
 
 <head>
-  <title>Saasta.fi statistics</title>
+<title>Saasta.fi statistics</title>
 </head>
 
 <body>
+
 <h1>saasta.fi statistics page</h1>
 
-<p>Page created on: <?php print date("m/d/Y"); ?></p>
+<?php
+for ($year = 2008; $year >= 2007; $year--)
+	for ($q = 4; $q >= 1; $q--) {
+?>
+<p>
 <table>
-  <tr><th>Descr</th><th>Value</th></tr>
-  <tr><td>Total # of posts</td><td><?php print $n_posts[0]->num_posts; ?></td></tr>
+<tr>
+<th colspan="2"><?php echo "Q".$q."/".$year; ?></th>
+<tr>
+<th>name</th>
+<th>num posts</th>
+</tr>
+<?php
+
+$foo = query_num_posts_in_quarter($q, $year);
+$totalPosts = 0;
+foreach ($foo as $f) {
+	print "<tr>";
+	print "<td>".$f->name."</td>";
+	print "<td>".$f->num_posts."</td>";
+	print "</tr>";
+
+	$totalPosts += $f->num_posts;
+}
+
+print "<tr><td><b>total:</b></td><td><b>".$totalPosts."</b></td></tr>";
+
+?>
 </table>
+</p>
+<?php } ?>
 
 </body>
-
 </html>
+
+
+
+
 
