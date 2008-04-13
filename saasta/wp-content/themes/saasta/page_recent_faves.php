b1a7cedb7926601076e6e7ec34c1b86c350bf1c3
jjhellst <jjhellst@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-04-13 15:39:02 +0000

    Add Recent Faves page template stub

---
+++ saasta/wp-content/themes/saasta/page_recent_faves.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Recent Faves
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php
+
+<div id="content" class="narrowcolumn">
+
+<h2>Recently Faved Posts</h2>
+
+</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
