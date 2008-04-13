f1600734ecb4adc8f9e1b0c86b6f487817565e9a
jjhellst <jjhellst@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-04-13 16:00:05 +0000

    Implement Recently faved posts page template.  Cosmetic changes

--- saasta/wp-content/themes/saasta/page_recent_faves.php
+++ saasta/wp-content/themes/saasta/page_recent_faves.php
@@ -6,11 +6,20 @@ Template Name: Recent Faves
 
 <?php get_header(); ?>
 
-<?php
-
 <div id="content" class="narrowcolumn">
 
 <h2>Recently Faved Posts</h2>
+
+<ul>
+<?php
+$foo = saasta_list_recent_faves(30);
+
+foreach ($foo as $f) {
+	print '<li><a href="'.$f->url.'">'.$f->title.'</a> ('.$f->num_faves.')</li>';
+    print "\n";
+}
+?>
+</ul>
 
 </div>
 
