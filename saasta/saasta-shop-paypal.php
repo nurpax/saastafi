215a15de721b3adfc73ec4dc309a8a33fd617b74
jjhellst <jjhellst@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-07-25 19:47:30 +0000

    Add PayPal button and PayPal payment notification handler

---
+++ saasta/saasta-shop-paypal.php
@@ -0,0 +1,11 @@
+<?php
+
+require( dirname(__FILE__) . '/wp-config.php' );
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+require_once(ABSPATH . '/saasta-common.php');
+
+nocache_headers();
+
+/* Process PayPal payment notification */
+
+?>
