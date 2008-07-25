716aa42051eb5ea884129e4ca86ddd497496be21
jjhellst <jjhellst@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-07-25 16:08:25 +0000

    First working implementation of the basic webshop order input form

--- trunk/saasta/wp-content/themes/saasta/shop.php
+++ trunk/saasta/wp-content/themes/saasta/shop.php
@@ -5,7 +5,7 @@ Template Name: Saasta Shop
 The purpose of this page template is to serve as an purchase order (PO) 
 input form.
 
-The purpose of this form & its associtaed logic is as follows:
+The purpose of this form & its associated logic is as follows:
 
 1) Input POs from customers
 
@@ -29,6 +29,36 @@ Database handling & validation code is located in /saasta/saasta-shop.php.
 
 require_once(ABSPATH . '/saasta-common.php');
 
+function saasta_confirm_order($ext_id)
+{
+    global $saasta_products, $wpdb;
+
+    $tbl_orders = $wpdb->prefix . "orders";
+
+    $i = $wpdb->escape($ext_id);
+    $q = $wpdb->get_row("SELECT email,id,order_state,address FROM $tbl_orders WHERE order_ext_uid='".$i."'");
+
+    if ($q)
+    {
+        // Mark new orders as 'confirmed':
+        if ($q->order_state == 'inbox') 
+        {
+            $wpdb->query("UPDATE $tbl_orders SET order_state = 'confirmed' WHERE id = $q->id");
+        }
+        
+        // Let the user know about his confirmation:
+        if ($q->order_state == 'inbox' || $q->order_state == 'confirmed')
+        {
+            echo "<h2>Order #$q->id confirmed and waiting for payment</h2>";
+            echo "<p>We're waiting to confirm that your order has been successfully paid for.  Once we can verify that the payment for the purchase was successful, we will ship the product to the below address:</p>";
+            echo "<pre style=\"font-size:1.2em\">$q->address</pre>";
+        }
+    } else
+    {
+        echo '<h2>Unknown order!</h2>';
+    }
+}
+
 function saasta_shop_form()
 {
     global $saasta_products;
@@ -47,7 +77,7 @@ function saasta_shop_form()
         print '<tr><td><img src="http://www.saasta.fi/saasta/people/unknown.png"/></td>';
         print '<td>' . $n . '</td>';
         
-        print '<td><select style="width:60px" value="qty" name="' . $id . '">';
+        print '<td><select style="width:60px" name="product_' . $id . '">';
         print '<option selected value="0">None</option>';
         print '<option value="50">50</option>';
         print '<option value="100">100</option>';
@@ -56,7 +86,9 @@ function saasta_shop_form()
         print '</tr>';
     } 
 ?>
-    <tr><td></td><td><input type="submit" value="Checkout!"/></td></tr>
+    <tr><td/><td>E-mail (*)</td><td><input name="email" type="text"/></td></tr>
+    <tr><td/><td>Delivery address (*)</td><td><textarea rows="5" cols="40" name="address"></textarea></td></tr>
+    <tr><td></td><td></td><td><input type="submit" value="Place order!"/></td></tr>
     </table>
     </form>
 <?php
@@ -70,13 +102,21 @@ function saasta_shop_form()
 <p>Please confirm your purchase order by following the instructions that you were just sent to via an automated e-mail.</p>
 <p><a href="<?php echo attribute_escape($_SERVER['REQUEST_URI']);?>">Back to the shop</a></p>
 
-<?php } else { ?>
+<?php 
+} elseif (isset($_GET['confirm_id'])) {
+
+saasta_confirm_order($_GET['confirm_id']);
+
+} else {
+?>
 <h2>Saasta Merchandise Shop</h2>
 
 <h3>Place your order for the below products</h3>
 <br/>
-<?php saasta_shop_form(); ?>
-<?php } ?>
+<?php
+saasta_shop_form();
+}
+?>
 
 </div>
 
