c36d098bda00bca6333c1d63b57a94c2f12f3ac6
jjhellst <jjhellst@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-07-27 21:39:05 +0000

    Automatically update order price from item quantity selectors with JavaScript

---
+++ saasta/wp-content/themes/saasta/saasta.js
@@ -0,0 +1,95 @@
+
+/* Saasta Javascript */
+
+function addLoadEvent(func) 
+{
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') 
+    {
+        window.onload = func;
+    } else 
+    {
+        window.onload = 
+            function() 
+            {
+                if (oldonload) {
+                    oldonload();
+                }
+                func();
+            }
+    }
+}
+
+function findProductElems(base,elemName)
+{
+    var inputs = document.getElementsByTagName(elemName);
+    var re = new RegExp("^"+base+"_([0-9]+)$");
+    var r = new Array();
+
+    for (var i = 0; i < inputs.length; i++)
+    {
+        var m = re.exec(inputs[i].name);
+        if (m != null)
+            r[m[1]] = inputs[i];
+    }
+
+    return r;
+}
+
+function computeOrderPrice(productSelects, unitPrices)
+{
+    var price = 0;
+
+    for (var i = 0; i < productSelects.length; i++)
+    {
+        var sel = productSelects[i];
+        var unitPrice = unitPrices[i];
+
+        if (sel != null)
+        {
+            var o = sel.options[sel.selectedIndex];
+
+            if (o != null && unitPrice != null)
+            {
+                price += o.value * unitPrice.value;
+            } else
+            {
+                alert("JS error: no unit price found for product #"+i);
+            }
+        }
+    }
+
+    return price;
+}
+
+function updatePrice(sels, unitPrices)
+{
+    var priceElem = document.getElementById('order_price');
+    // Compute order price from select elements:
+    var orderPrice = computeOrderPrice(sels, unitPrices);
+    var d = Math.floor(orderPrice / 100);
+    var f = Math.floor(Math.floor(orderPrice % 100));
+
+    priceElem.innerHTML = d + '.' + f;
+}
+
+function registerPriceCompute()
+{
+    var productSelects = findProductElems("product", "select");
+    var productUnitPrices = findProductElems("unit_price_product", "input");
+    var priceUpdateFunc = 
+        function() { updatePrice(productSelects, productUnitPrices); };
+
+    for (var i = 0; i < productSelects.length; i++)
+    {
+        var sel = productSelects[i];
+
+        // Register change handlers for all selectors.  When any of
+        // the selectors change, price text gets automatically
+        // updated.
+        if (sel != null)
+            sel.onchange = priceUpdateFunc;
+    }
+
+    priceUpdateFunc();
+}
