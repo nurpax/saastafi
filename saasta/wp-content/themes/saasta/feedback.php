c1c3dc241b3150224fcb84f2021c4f8873658f6d
jjhellst <jjhellst@4e28f69c-3835-0410-a477-bf44b290d73f>
2008-08-22 17:47:50 +0000

    Feedback Form -> Suggestion box.  Less coderish

--- saasta/wp-content/themes/saasta/feedback.php
+++ saasta/wp-content/themes/saasta/feedback.php
@@ -17,12 +17,15 @@ function saasta_print_feedback_form()
     print '<strong>Subject: </strong><br/><input size="70" type="text" name="subject"/><br/><br/>';
     print '<strong>Text:</strong><br/><textarea rows="20" cols="60" name="body"></textarea>';
     print '<br />';
-    print '<input type="submit" style="border:1px solid black;font-size:smaller;background-color:#ddd391" value="Send"/>';
+    print '<input type="submit" style="border:1px solid black;font-size:smaller;" value="Send"/>';
     print '</form>';
 }
 ?>
 
 <div id="content" class="narrowcolumn">
+
+<h2>Suggestion Box</h2>
+<br/>
 
 <?php if (isset($_GET['saved'])) { ?>
 <p><strong>Feedback sent.  Thank you!</strong></p>
@@ -42,8 +45,7 @@ function saasta_print_feedback_form()
 ?>
 
 <?php if (!isset($_GET['saved'])) { ?>
-<h2>Feedback to the Saasta Administration</h2>
-<p>Send anonymous feedback on our site.  Improvement suggestions.. DIS..? RES! etc.</p>
+<p>Send anonymous feedback on our site.  Improvement suggestions.. DIS..? RES! You tell us.</p>
 
 <p>If you wish to be contacted by our officials, remember to include your e-mail address in your letter.</p>
 
